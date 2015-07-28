8eae482088466790349251f0852d7f8c04d1cefc
Corentin Cadiou <ccadiou@linagora.com>
2015-07-28 14:26:31 +0200

    Add grunt tasks for building for the browser

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,31 @@ module.exports = function(grunt) {
         },
         src: ['test/test.js']
       }
+    },
+    browserify: {
+      main: {
+        src: 'lib/rbtree_by_index.js',
+        dest: 'dist/rbtree_by_index.js'
+      },
+      options: {
+        browserifyOptions: {
+          standalone: 'RBTree'
+        }
+      }
+    },
+    uglify: {
+      main: {
+        src: 'dist/rbtree_by_index.js',
+        dest: 'dist/rbtree_by_index.min.js'
+      }
     }
+
   });
 
   grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-browserify');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['mochaTest']);
+  grunt.registerTask('build', ['browserify', 'uglify']);
+  grunt.registerTask('default', ['mochaTest', 'build']);
 };
